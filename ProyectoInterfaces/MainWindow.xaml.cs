815c6073f30fe40b5dc9fed9192a27d515722516
javi <javiplus27@gmail.com>
2023-01-27 01:53:40 +0100

    2ª Actualización  de la interfaz.

--- ProyectoInterfaces/MainWindow.xaml.cs
+++ ProyectoInterfaces/MainWindow.xaml.cs
@@ -25,5 +25,22 @@ namespace ProyectoInterfaces
             InitializeComponent();
         }
 
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton==MouseButtonState.Pressed) 
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
