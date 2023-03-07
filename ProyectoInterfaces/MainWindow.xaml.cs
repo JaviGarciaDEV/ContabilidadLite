f81906bc9e0cf7dbf61933242202126a05a332b5
javi <javiplus27@gmail.com>
2023-03-07 22:55:39 +0100

    Aplicación guarda el tema establecido y lo carga cuando se inicia de nuevo.

--- ProyectoInterfaces/MainWindow.xaml.cs
+++ ProyectoInterfaces/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ProyectoInterfaces
 {
@@ -24,6 +25,22 @@ namespace ProyectoInterfaces
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Comprobar que tema estaba establecido
+            string contenido = LeerArchivo(@"config.txt");
+            if (contenido == "dark")
+            {
+                pintarNegro();
+                darkTheme = true;
+            } else
+            {
+                pintarAzul();
+                darkTheme = false;
+            }
 
         }
 
@@ -57,6 +74,17 @@ namespace ProyectoInterfaces
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+        private void btnMaxmimize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
         }
 
         private void ButtonCalcular_click(object sender, RoutedEventArgs e)
@@ -99,167 +127,200 @@ namespace ProyectoInterfaces
 
         private void Button_Theme(object sender, RoutedEventArgs e)
         {
+            string texto;
+            string rutaArchivo;
+
             if (darkTheme == true)
             {
                 darkTheme = false;
-                // Crear un nuevo SolidColorBrush con el color deseado
-                var colorFondo = (Color)ColorConverter.ConvertFromString("#FFFFFF");
-                var colorAzulContenedores = (Color)ColorConverter.ConvertFromString("#9ACAEE");
-                var colorAzulBotones = (Color)ColorConverter.ConvertFromString("#76AFD7");
+                pintarAzul();
+                texto = "azul";
+                rutaArchivo = @"config.txt";
+                EscribirEnArchivo(texto, rutaArchivo);
 
-                // Cambio color fondo y contenedores
-                var brush = new SolidColorBrush(colorFondo);
-                ventana.Background = brush;
-                brush = new SolidColorBrush(colorAzulContenedores);
-                menu.Background = brush;
-                contenedor.Background = brush;
-
-                // Cambio color titulos
-                titulo.Foreground = brush;
-
-                // Cambio color botones menu
-                brush = new SolidColorBrush(colorAzulBotones);
-                botonCasa.Background = brush;
-                var border = (Border)botonCasa.Template.FindName("border", botonCasa);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)botonFacturas.Template.FindName("border", botonFacturas);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)botonOcio.Template.FindName("border", botonOcio);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)botonPagas.Template.FindName("border", botonPagas);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                // Cambio botones contenedor
-                border = (Border)comida.Template.FindName("border", comida);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)coche.Template.FindName("border", coche);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)luz.Template.FindName("border", luz);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)alquiler.Template.FindName("border", alquiler);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)botonCalcular.Template.FindName("border", botonCalcular);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)ingresos.Template.FindName("border", ingresos);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)restante.Template.FindName("border", restante);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                border = (Border)gastoTotalCont.Template.FindName("border", gastoTotalCont);
-                border.Background = new SolidColorBrush(colorAzulBotones);
-
-                numIngresos.Background = brush;
-                gastoComida.Background = brush;
-                gastoCoche.Background = brush;
-                gastoLuz.Background = brush;
-                gastoAlquiler.Background = brush;
-
-                // Obtener la imagen actual
-                Image image = (Image)buttonTheme.Template.FindName("image", buttonTheme);
-                image.Source = new BitmapImage(new Uri("temaoscuro.png", UriKind.Relative));
-
-                //Cambio la barra
-                barra.Background = new SolidColorBrush(colorAzulContenedores);
-                btnClose.Opacity = 0.8;
-                imageMax.Opacity = 0.78;
             }
             else
             {
                 darkTheme = true;
-                // Crear un nuevo SolidColorBrush con el color deseado
-                var colorFondo = (Color)ColorConverter.ConvertFromString("#FF1B1B1B");
-                var colorContenedores = (Color)ColorConverter.ConvertFromString("#FF303030");
-                var colorBotones = (Color)ColorConverter.ConvertFromString("#FF1B1B1B");
-                var colorTitulo = (Color)ColorConverter.ConvertFromString("#FFFFFF");
-
-                // Cambio color fondo y contenedores
-                var brush = new SolidColorBrush(colorFondo);
-                ventana.Background = brush;
-                brush = new SolidColorBrush(colorContenedores);
-                menu.Background = brush;
-                contenedor.Background = brush;
-
-                // Cambio color titulos
-                brush = new SolidColorBrush(colorTitulo);
-                titulo.Foreground = brush;
-
-                // Cambio color botones menu
-                brush = new SolidColorBrush(colorBotones);
-                botonCasa.Background = brush;
-                var border = (Border)botonCasa.Template.FindName("border", botonCasa);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)botonFacturas.Template.FindName("border", botonFacturas);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)botonOcio.Template.FindName("border", botonOcio);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)botonPagas.Template.FindName("border", botonPagas);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                // Cambio botones contenedor
-                border = (Border)comida.Template.FindName("border", comida);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)coche.Template.FindName("border", coche);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)luz.Template.FindName("border", luz);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)alquiler.Template.FindName("border", alquiler);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)botonCalcular.Template.FindName("border", botonCalcular);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)ingresos.Template.FindName("border", ingresos);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)restante.Template.FindName("border", restante);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                border = (Border)gastoTotalCont.Template.FindName("border", gastoTotalCont);
-                border.Background = new SolidColorBrush(colorBotones);
-
-                numIngresos.Background = brush;
-                gastoComida.Background = brush;
-                gastoCoche.Background = brush;
-                gastoLuz.Background = brush;
-                gastoAlquiler.Background = brush;
-
-                // Obtener la imagen actual
-                Image image = (Image)buttonTheme.Template.FindName("image", buttonTheme);
-                image.Source = new BitmapImage(new Uri("tema.png", UriKind.Relative));
-
-                //Cambio la barra
-                barra.Background = new SolidColorBrush(colorContenedores);
-                btnClose.Opacity = 0.455;
-                imageMax.Opacity = 0.43;
+                pintarNegro();
+                texto = "dark";
+                rutaArchivo = @"config.txt";
+                EscribirEnArchivo(texto, rutaArchivo);
             }
 
         }
 
-        private void btnMaxmimize_Click(object sender, RoutedEventArgs e)
+        public void pintarAzul()
         {
-            if (WindowState == WindowState.Maximized)
+            // Crear un nuevo SolidColorBrush con el color deseado
+            var colorFondo = (Color)ColorConverter.ConvertFromString("#FFFFFF");
+            var colorAzulContenedores = (Color)ColorConverter.ConvertFromString("#9ACAEE");
+            var colorAzulBotones = (Color)ColorConverter.ConvertFromString("#76AFD7");
+
+            // Cambio color fondo y contenedores
+            var brush = new SolidColorBrush(colorFondo);
+            ventana.Background = brush;
+            brush = new SolidColorBrush(colorAzulContenedores);
+            menu.Background = brush;
+            contenedor.Background = brush;
+
+            // Cambio color titulos
+            titulo.Foreground = brush;
+
+            // Cambio color botones menu
+            brush = new SolidColorBrush(colorAzulBotones);
+            botonCasa.Background = brush;
+            var border = (Border)botonCasa.Template.FindName("border", botonCasa);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)botonFacturas.Template.FindName("border", botonFacturas);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)botonOcio.Template.FindName("border", botonOcio);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)botonPagas.Template.FindName("border", botonPagas);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            // Cambio botones contenedor
+            border = (Border)comida.Template.FindName("border", comida);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)coche.Template.FindName("border", coche);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)luz.Template.FindName("border", luz);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)alquiler.Template.FindName("border", alquiler);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)botonCalcular.Template.FindName("border", botonCalcular);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)ingresos.Template.FindName("border", ingresos);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)restante.Template.FindName("border", restante);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            border = (Border)gastoTotalCont.Template.FindName("border", gastoTotalCont);
+            border.Background = new SolidColorBrush(colorAzulBotones);
+
+            numIngresos.Background = brush;
+            gastoComida.Background = brush;
+            gastoCoche.Background = brush;
+            gastoLuz.Background = brush;
+            gastoAlquiler.Background = brush;
+
+            // Obtener la imagen actual
+            Image image = (Image)buttonTheme.Template.FindName("image", buttonTheme);
+            image.Source = new BitmapImage(new Uri("temaoscuro.png", UriKind.Relative));
+
+            //Cambio la barra
+            barra.Background = new SolidColorBrush(colorAzulContenedores);
+            btnClose.Opacity = 0.8;
+            imageMax.Opacity = 0.78;
+        }
+
+        public void pintarNegro()
+        {
+            // Crear un nuevo SolidColorBrush con el color deseado
+            var colorFondo = (Color)ColorConverter.ConvertFromString("#FF1B1B1B");
+            var colorContenedores = (Color)ColorConverter.ConvertFromString("#FF303030");
+            var colorBotones = (Color)ColorConverter.ConvertFromString("#FF1B1B1B");
+            var colorTitulo = (Color)ColorConverter.ConvertFromString("#FFFFFF");
+
+            // Cambio color fondo y contenedores
+            var brush = new SolidColorBrush(colorFondo);
+            ventana.Background = brush;
+            brush = new SolidColorBrush(colorContenedores);
+            menu.Background = brush;
+            contenedor.Background = brush;
+
+            // Cambio color titulos
+            brush = new SolidColorBrush(colorTitulo);
+            titulo.Foreground = brush;
+
+            // Cambio color botones menu
+            brush = new SolidColorBrush(colorBotones);
+            botonCasa.Background = brush;
+            var border = (Border)botonCasa.Template.FindName("border", botonCasa);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)botonFacturas.Template.FindName("border", botonFacturas);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)botonOcio.Template.FindName("border", botonOcio);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)botonPagas.Template.FindName("border", botonPagas);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            // Cambio botones contenedor
+            border = (Border)comida.Template.FindName("border", comida);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)coche.Template.FindName("border", coche);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)luz.Template.FindName("border", luz);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)alquiler.Template.FindName("border", alquiler);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)botonCalcular.Template.FindName("border", botonCalcular);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)ingresos.Template.FindName("border", ingresos);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)restante.Template.FindName("border", restante);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            border = (Border)gastoTotalCont.Template.FindName("border", gastoTotalCont);
+            border.Background = new SolidColorBrush(colorBotones);
+
+            numIngresos.Background = brush;
+            gastoComida.Background = brush;
+            gastoCoche.Background = brush;
+            gastoLuz.Background = brush;
+            gastoAlquiler.Background = brush;
+
+            // Obtener la imagen actual
+            Image image = (Image)buttonTheme.Template.FindName("image", buttonTheme);
+            image.Source = new BitmapImage(new Uri("tema.png", UriKind.Relative));
+
+            //Cambio la barra
+            barra.Background = new SolidColorBrush(colorContenedores);
+            btnClose.Opacity = 0.455;
+            imageMax.Opacity = 0.43;
+        }
+
+        public void EscribirEnArchivo(string texto, string rutaArchivo)
+        {
+            // Comprobamos si el archivo ya existe
+            if (!File.Exists(rutaArchivo))
             {
-                WindowState = WindowState.Normal;
+                // Si no existe, lo creamos
+                using (FileStream fs = File.Create(rutaArchivo)) { }
             }
-            else
+
+            // Sobreescribimos el contenido del archivo con el nuevo texto
+            File.WriteAllText(rutaArchivo, texto);
+        }
+
+        public string LeerArchivo(string rutaArchivo)
+        {
+            // Comprobamos si el archivo existe
+            if (!File.Exists(rutaArchivo))
             {
-                WindowState = WindowState.Maximized;
+                throw new FileNotFoundException($"El archivo {rutaArchivo} no existe.");
             }
+
+            // Leemos el contenido del archivo y lo devolvemos como un string
+            return File.ReadAllText(rutaArchivo);
         }
 
     }
